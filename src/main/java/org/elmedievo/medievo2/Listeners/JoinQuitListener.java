0f5abc55dbf5c2dce3b1908cacbabf9f8e5a040b
Jorge Orlando <bgmp00@protonmail.com>
2019-10-18 00:45:53 -0300

    Implement the welcome message

--- src/main/java/org/elmedievo/medievo2/Listeners/JoinQuitListener.java
+++ src/main/java/org/elmedievo/medievo2/Listeners/JoinQuitListener.java
@@ -10,6 +10,8 @@ import org.bukkit.event.player.PlayerQuitEvent;
 import org.elmedievo.medievo2.Configuration;
 import org.elmedievo.medievo2.Medievo2;
 
+import java.util.List;
+
 public class JoinQuitListener implements Listener {
     @EventHandler(priority = EventPriority.HIGHEST)
     public void onJoin(PlayerJoinEvent event) {
@@ -23,6 +25,24 @@ public class JoinQuitListener implements Listener {
         Medievo2.getRankDatabase.deliverRanksToPlayer(player);
 
         event.setJoinMessage(colorFormattedJoinMessage);
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onJoin2(PlayerJoinEvent event) {
+        Configuration configuration = Medievo2.getConfiguration;
+        List<String> welcomeMessage = configuration.getWelcome_message();
+        Player player = event.getPlayer();
+
+        StringBuilder welcomeMessageBuilder = new StringBuilder();
+
+        welcomeMessage.forEach(line -> {
+            String finalLine = line
+                    .replaceAll("%player%", player.getDisplayName());
+            String coloredFinalLine = ChatColor.translateAlternateColorCodes('&', finalLine);
+            welcomeMessageBuilder.append(coloredFinalLine).append("\n");
+        });
+
+        player.sendMessage(welcomeMessageBuilder.toString());
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
