d844255b925c6da8d887edb3d7159ff8632f7921
BGM <bgmp00@protonmail.com>
2019-10-22 23:52:12 -0300

    Update broadcast permission

--- src/main/java/org/elmedievo/medievo2/Utils/Permission.java
+++ src/main/java/org/elmedievo/medievo2/Utils/Permission.java
@@ -6,7 +6,7 @@ public enum Permission {
     CHAT_COLOR("medievo2.chat.color", "Chat color permission."),
     CHAT_PM("medievo2.chat.message", "Allows permission to send private messages."),
     CHAT_REPLY("medievo2.chat.reply", "Allows permission to reply the last private message received."),
-    CHAT_BROADCAST("medievo2.core.broadcast", "Allows access to broadcast messages."),
+    CHAT_BROADCAST("medievo2.chat.broadcast", "Allows access to broadcast messages."),
     RANK("medievo2.rank", "Rank command node permission."),
     RANK_ADD("medievo2.rank.add", "Allows permission to add ranks to players."),
     RANK_REMOVE("medievo2.rank.remove", "Allows permission to remove ranks from players."),
